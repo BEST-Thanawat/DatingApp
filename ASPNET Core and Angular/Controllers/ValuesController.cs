b20a47ffbdee62963f1bfebe8b4ee7637374da1b
BEST-Thanawat <thanawat.sukwibul@gmail.com>
2020-05-19 20:54:39 -0700

    Add project files.

---
+++ ASPNET Core and Angular/Controllers/ValuesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASPNET_Core_and_Angular.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNET_Core_and_Angular.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private readonly Angular_Core_DBContext _context;
+
+        public ValuesController(Angular_Core_DBContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(value);
+        }
+
+        //[HttpPost]
+        //public  
+    }
+}
---
+++ ASPNET Core and Angular/Models/Angular_Core_DBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNET_Core_and_Angular.Models
+{
+    public class Angular_Core_DBContext : DbContext
+    {
+        public Angular_Core_DBContext(DbContextOptions<Angular_Core_DBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
