0e869900dc4a299b777859e9a94036db883818bd
BEST-Thanawat <thanawat.sukwibul@gmail.com>
2020-05-23 22:03:44 -0700

    Update Services.

--- ASPNET Core and Angular/ClientApp/src/app/app.module.ts
+++ ASPNET Core and Angular/ClientApp/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
@@ -28,6 +30,8 @@ import { ErrorInterceptorProvider } from './_services/error.interceptor';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot(),
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
